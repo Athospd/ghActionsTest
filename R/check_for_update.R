e4867b5607a586c986bfe0bec676f6298b10852c
Athos Damiani <athos.damiani@gmail.com>
2020-05-07 01:36:55 -0300

    workflow encrypted

---
+++ R/check_for_update.R
@@ -0,0 +1,24 @@
+library(magrittr)
+library(httr)
+library(stringr)
+library(xml2)
+library(glue)
+channel="#geral"
+incoming_webhook_url= Sys.getenv("INCOMING_WEBHOOK_URL")
+
+hour <- RETRY("GET", "https://time.is/", write_disk("hour.html", overwrite = TRUE))
+
+hour_content <- content(hour)
+watched_content <- hour_content %>%
+  xml_find_first('//*[@id="clock"]') %>%
+  xml_text() %>%
+  str_extract(":([0-9]{2}):")
+
+current_content <- readRDS("current_content.rds")
+if(current_content != watched_content) {
+  msg <- glue("The content has changed from {current_content} to  {watched_content}. Checked at {Sys.time()}")
+  current_content <- watched_content
+  saveRDS(current_content, "current_content.rds")
+
+  POST(incoming_webhook_url, body = list(text = msg), encode = "json")
+}
